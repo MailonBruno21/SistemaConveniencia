21ccac62ae193047425df362ac10215ae5de080e
igor_ <igor_@DESKTOP-Q9BHHRI>
2020-02-24 21:51:04 -0400

    BD conectando

--- src/dao/FuncionarioDAO.java
+++ src/dao/FuncionarioDAO.java
@@ -13,112 +13,109 @@ public class FuncionarioDAO {
     public FuncionarioDAO(Connection conexao) {
         this.conexaoFuncionario = conexao;
     }
+
     /*
     * INSERIR FUNCIONARIO  AO BANCO DE DADOS
-    */
+     */
     public void insert(Funcionario funcionario) throws SQLException {
-        
+
         String text = funcionario.getNome();
 
         if (!"".equals(text)) {
-            String sql = "insert into funcionario(nome) values('"+funcionario.getNome()+"');";
-        }else{
+            String sql = "insert into funcionario(nome) values('" + funcionario.getNome() + "');";
+        } else {
             //Nome nao pode ficar vazio
         }
-        
+
         text = funcionario.getUsuario();
         if (!"".equals(text)) {
-            String sql = "insert into funcionario(nome) values('"+funcionario.getUsuario()+"');";
+            String sql = "insert into funcionario(nome) values('" + funcionario.getUsuario() + "');";
         }
-        
+
         text = funcionario.getDataContradado();
         if (!"".equals(text)) {
-            String sql = "insert into funcionario(nome) values('"+funcionario.getDataContradado()+"');";
+            String sql = "insert into funcionario(nome) values('" + funcionario.getDataContradado() + "');";
         }
-        
+
         text = funcionario.getEndereco();
         if (!"".equals(text)) {
-            String sql = "insert into funcionario(nome) values('"+funcionario.getEndereco()+"');";
+            String sql = "insert into funcionario(nome) values('" + funcionario.getEndereco() + "');";
         }
-        
+
         text = funcionario.getBairro();
         if (!"".equals(text)) {
-            String sql = "insert into funcionario(nome) values('"+funcionario.getBairro()+"');";
+            String sql = "insert into funcionario(nome) values('" + funcionario.getBairro() + "');";
         }
-        
+
         text = funcionario.getUf();
         if (!"".equals(text)) {
-            String sql = "insert into funcionario(nome) values('"+funcionario.getUf()+"');";
+            String sql = "insert into funcionario(nome) values('" + funcionario.getUf() + "');";
         }
-        
+
         text = funcionario.getCidade();
         if (!"".equals(text)) {
-            String sql = "insert into funcionario(nome) values('"+funcionario.getCidade()+"');";
+            String sql = "insert into funcionario(nome) values('" + funcionario.getCidade() + "');";
         }
-        
+
         text = funcionario.getEmail();
         if (!"".equals(text)) {
-            String sql = "insert into funcionario(nome) values('"+funcionario.getEmail()+"');";
+            String sql = "insert into funcionario(nome) values('" + funcionario.getEmail() + "');";
         }
-        
+
         double numDouble = funcionario.getComissao();
         if (!"".equals(text)) {
-            String sql = "insert into funcionario(nome) values('"+funcionario.getComissao()+"');";
+            String sql = "insert into funcionario(nome) values('" + funcionario.getComissao() + "');";
         }
-        
+
         int numInt = funcionario.getCelularUm();
         if (!"".equals(text)) {
-            String sql = "insert into funcionario(nome) values('"+funcionario.getCelularUm()+"');";
+            String sql = "insert into funcionario(nome) values('" + funcionario.getCelularUm() + "');";
         }
-        
+
         numInt = funcionario.getCelularDois();
         if (!"".equals(text)) {
-            String sql = "insert into funcionario(nome) values('"+funcionario.getCelularDois()+"');";
+            String sql = "insert into funcionario(nome) values('" + funcionario.getCelularDois() + "');";
         }
-        
+
         numInt = funcionario.getCep();
         if (!"".equals(text)) {
-            String sql = "insert into funcionario(nome) values('"+funcionario.getCep()+"');";
+            String sql = "insert into funcionario(nome) values('" + funcionario.getCep() + "');";
         }
-        
+
         numDouble = funcionario.getSalario();
         if (!"".equals(text)) {
-            String sql = "insert into funcionario(nome) values('"+funcionario.getSalario()+"');";
+            String sql = "insert into funcionario(nome) values('" + funcionario.getSalario() + "');";
         }
-        
+
         numInt = funcionario.getCpf();
         if (!"".equals(text)) {
-            String sql = "insert into funcionario(nome) values('"+funcionario.getCpf()+"');";
+            String sql = "insert into funcionario(nome) values('" + funcionario.getCpf() + "');";
         }
-        
+
         numInt = funcionario.getRg();
         if (!"".equals(text)) {
-            String sql = "insert into funcionario(nome) values('"+funcionario.getRg()+"');";
+            String sql = "insert into funcionario(nome) values('" + funcionario.getRg() + "');";
         }
-        
+
         text = funcionario.getEndereco();
         if (!"".equals(text)) {
-            String sql = "insert into funcionario(nome) values('"+funcionario.getEndereco()+"');";
+            String sql = "insert into funcionario(nome) values('" + funcionario.getEndereco() + "');";
         }
-        
+
         numInt = funcionario.getPermissao();
         if (!"".equals(text)) {
-            String sql = "insert into funcionario(nome) values('"+funcionario.getPermissao()+"');";
+            String sql = "insert into funcionario(nome) values('" + funcionario.getPermissao() + "');";
         }
-        
+
         text = funcionario.getUsuario();
         if (!"".equals(text)) {
-            String sql = "insert into funcionario(nome) values('"+funcionario.getUsuario()+"');";
+            String sql = "insert into funcionario(nome) values('" + funcionario.getUsuario() + "');";
         }
-        
+
         text = funcionario.getSenha();
         if (!"".equals(text)) {
-            String sql = "insert into funcionario(nome) values('"+funcionario.getSenha()+"');";
+            String sql = "insert into funcionario(nome) values('" + funcionario.getSenha() + "');";
         }
-        
-        
-        
-        
 
         String sql = "insert into funcionario(usuario,senha) values('Roberto','123');";
 
@@ -126,20 +123,17 @@ public class FuncionarioDAO {
         statement.execute();
 
     }
-    
- 
 
     public boolean autenticarLoginFuncionario(Funcionario funcionario) throws SQLException {
 
-        String sql = "select * from funcionario where usuario = '"+funcionario.getUsuario()+"' and senha = '"+funcionario.getSenha()+"';";
+        String sql = "select * from funcionario where username = '" + funcionario.getUsuario() + "' and senha = '" + funcionario.getSenha() + "';";
         PreparedStatement statement = conexaoFuncionario.prepareStatement(sql);
         statement.execute();
-        
+
         ResultSet resultSet = statement.getResultSet();
-        
+
         return resultSet.next();
-        
-        
+
     }
 
 }
